bf50a82ee125e5f64c8c4aa996ab6e90c26a1af0
bedenchw30 <90423350+bedenchw30@users.noreply.github.com>
2021-09-13 21:30:13 -0400

    initial commit

--- src/main/java/exerciseTwelve.java
+++ src/main/java/exerciseTwelve.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+
 /**
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 beden chew
  */
+import java.util.Scanner;
 public class exerciseTwelve {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -17,6 +18,6 @@ public class exerciseTwelve {
         double numOfYears = input.nextDouble();
 
         double investmentTotal = (principal*(1+rateOfInterest*.01*numOfYears));
-        System.out.println("After " +numOfYears+ " at " + rateOfInterest +"%, the investment will be worth $"+Math.round(investmentTotal*100)/100.0 +".");
+        System.out.println("After " +numOfYears+ " years at " + rateOfInterest +"%, the investment will be worth $"+Math.round(investmentTotal*100)/100.0 +".");
     }
 }

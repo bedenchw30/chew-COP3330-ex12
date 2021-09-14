26a426f43393482a25f1614d90a63d8b7247d1ae
bedenchw30 <90423350+bedenchw30@users.noreply.github.com>
2021-09-13 20:13:35 -0400

    Initial commit

---
+++ src/main/java/exerciseTwelve.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 beden chew
+ */
+public class exerciseTwelve {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the principal: ");
+        double principal = input.nextDouble();
+
+        System.out.print("Enter the rate of interest: ");
+        double rateOfInterest = input.nextDouble();
+
+        System.out.print("Enter the number of years: ");
+        double numOfYears = input.nextDouble();
+
+        double investmentTotal = (principal*(1+rateOfInterest*.01*numOfYears));
+        System.out.println("After " +numOfYears+ " at " + rateOfInterest +"%, the investment will be worth $"+Math.round(investmentTotal*100)/100.0 +".");
+    }
+}
